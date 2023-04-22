f8f1dbb63a3e42f01f4221878cbbd0c91f291f46
weilong1989 <301012267@qq.com>
2023-04-22 19:06:36 +0800

    test2

---
+++ src/com/main/Test001.java
@@ -0,0 +1,14 @@
+package com.main;
+
+public class Test001 {
+    private static final String TAG = "Test001";
+    private static final String TEST;
+
+    static {
+        TEST = "test";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(TAG);
+    }
+}
