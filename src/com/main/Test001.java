1326b445ef4623f632d284d1072955944272862d
weilong1989 <301012267@qq.com>
2023-04-22 20:59:47 +0800

    123145

--- src/com/main/Test001.java
+++ src/com/main/Test001.java
@@ -3,7 +3,6 @@ package com.main;
 public class Test001 {
     private static final String TAG = "Test001";
     private static final String TEST;
-    private static final String TEST2 = null;
 
     static {
         TEST = "test";
